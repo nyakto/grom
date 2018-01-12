f87c1571b16d5d8b5480254d237f62a4c63a1861
Stanislav Yashenkov <stas@realtymag.ru>
2018-01-13 01:27:11 +0300

    Lexer: added tests

--- lexer/src/main/kotlin/com/github/nyakto/grom/lexer/Keywords.kt
+++ lexer/src/main/kotlin/com/github/nyakto/grom/lexer/Keywords.kt
@@ -1,10 +1,12 @@
 package com.github.nyakto.grom.lexer
 
-object Keywords {
+object Keywords : Iterable<String> {
     private val map = hashMapOf(
         "model" to TokenType.ModelKeyword,
         "view" to TokenType.ViewKeyword
     )
 
     operator fun get(word: String): TokenType? = map[word]
+
+    override fun iterator() = map.keys.iterator()
 }
--- lexer/src/test/kotlin/com/github/nyakto/grom/lexer/LexerTests.kt
+++ lexer/src/test/kotlin/com/github/nyakto/grom/lexer/LexerTests.kt
@@ -4,10 +4,68 @@ import org.junit.Assert
 import org.junit.Test
 
 class LexerTests {
+    private fun expectError(
+        expectedError: Throwable,
+        block: () -> Unit
+    ) {
+        var failed = true
+        try {
+            block()
+            failed = false
+        } catch (actualError: Throwable) {
+            Assert.assertTrue(
+                "expected ${expectedError::class.simpleName}, but got ${actualError::class.simpleName} exception",
+                actualError::class == expectedError::class
+            )
+            Assert.assertEquals(expectedError.message, actualError.message)
+        }
+        Assert.assertTrue("no errors occured, but ${expectedError::class.simpleName} expected", failed)
+    }
+
     @Test
-    fun testKeywords() {
+    fun `checkTokens have to check size`() {
+        expectError(AssertionError("expected less tokens")) {
+            checkTokens("model")
+        }
+        expectError(AssertionError("expected more tokens")) {
+            checkTokens("", TokenType.ModelKeyword)
+        }
+    }
+
+    @Test
+    fun `example model declaration`() {
+        checkTokens(
+            """
+                model {
+                }
+            """,
+            TokenType.ModelKeyword,
+            TokenType.LeftBrace,
+            TokenType.RightBrace
+        )
+    }
+
+    @Test
+    fun `example view declaration`() {
+        checkTokens(
+            """
+                view {
+                }
+            """,
+            TokenType.ViewKeyword,
+            TokenType.LeftBrace,
+            TokenType.RightBrace
+        )
+    }
+
+    @Test
+    fun `keywords have to be recognized`() {
         checkTokens("model", TokenType.ModelKeyword)
         checkTokens("view", TokenType.ViewKeyword)
+
+        Keywords.forEach { keyword ->
+            checkTokens(keyword, Keywords[keyword]!!)
+        }
     }
 
     private fun <Source, Item> checkTokens(
@@ -20,7 +78,7 @@ class LexerTests {
         val iterator = items.iterator()
         val lexer = LexerFactory.createLexer(object : LexerListener {
             override fun onToken(lexer: Lexer, token: Token) {
-                Assert.assertTrue(iterator.hasNext())
+                Assert.assertTrue("expected less tokens", iterator.hasNext())
                 val expected = iterator.next()
                 val actual = itemGetter(token)
                 Assert.assertEquals(expected, actual)
@@ -33,7 +91,7 @@ class LexerTests {
             }
         })
         lex(lexer, source)
-        Assert.assertFalse(iterator.hasNext())
+        Assert.assertFalse("expected more tokens", iterator.hasNext())
     }
 
     private fun checkTokens(
